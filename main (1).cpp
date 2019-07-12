7eb5f237cc998ff3aed5944ad9d161a70c074f74
Shahed Zaidi <46625168+szaidi94@users.noreply.github.com>
2019-07-12 11:42:16 +0200

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,84 @@
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Servo.h>
+#include <PubSubClient.h>
+
+// Initialize the client library
+WiFiClient espClient;
+//This library provides a client for doing simple publish/subscribe messaging with a server that supports MQTT.
+PubSubClient client(espClient);
+//This library allows an Arduino board to control RC (hobby) servo motors
+Servo servoObject;
+
+//This function will be called whenever a message arrive
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  payload[length]='\0'; //The end of a String
+  int a = atoi((const char*)payload); //Asci to Integer
+  servoObject.write(a);   
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    if (client.connect("client-sfasdfasdf")) {
+      Serial.println("connected");
+      client.publish("/status/devices", "I'm here");
+      
+      client.subscribe("/servo/1");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  servoObject.attach(2);
+  servoObject.write(0);
+  delay(2000);
+
+  Serial.begin(9600);
+  Serial.println("Hallo Welt");
+  //Setting WIFI mode to STA
+  WiFi.mode(WIFI_STA);
+  //Setting the host name
+  WiFi.hostname("dpsasdf");
+  /*
+  Connecting to WIFI 
+  username : hsb-labor
+  password : 6MVfNSqdMr5SZo6d
+  */
+  WiFi.begin("hsb-labor", "6MVfNSqdMr5SZo6d");
+
+  //While we're not connected , delay and try again
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  //Print the IP of the aruino
+  Serial.println(WiFi.localIP());
+  //Set the server and the callBack function
+  client.setServer("192.168.206.19", 1883);
+  client.setCallback(callback);
+}
+
+//This function will keep try to connect to the Arduino
+void loop() {
+  if (!client.connected())
+   reconnect();
+
+  client.loop();
+}
